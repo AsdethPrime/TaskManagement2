4c8ff287bceae3c67c34d8010cf84d90361ccde8
asdethprime <asdethprime@gmail.com>
2020-01-29 02:51:20 +0000

    Nav link is now set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, NavLink, HashRouter } from 'react-router-dom'
+import { AppBar, Typography, Divider, Grid, Button} from '@material-ui/core'
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const gridStyle = {
+    fontSize: 25,
+  }
+
+  const ShowTitle = ({title}) => (
+    <AppBar position='relative' align='center'>
+      <Typography variant='h4'>
+        {title}
+      </Typography>
+    </AppBar>
+  )
+
+  const ShowLink = ({url, name }) => (
+    <Grid item xs={4}>
+      <NavLink to={url}>
+        <Button variant='outlined' color='primary' fullWidth>
+          {name}
+        </Button>
+      </NavLink>
+    </Grid>
+  )
+
+  const ShowExactLink = ({url, name }) => (
+    <Grid item xs={4}>
+      <NavLink exact to={url}>
+        <Button variant='outlined' color='primary' fullWidth>
+          {name}
+        </Button>
+      </NavLink>
+    </Grid>
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <HashRouter>
+      <div>
+        <nav>
+          <ShowTitle title='Task Management App v2' />
+          <Divider />
+          { /* Show Navigation Link */}
+          <Grid container style={gridStyle}>
+            <ShowExactLink url='/' name='HOME' />
+            <ShowLink url='/engineers' name='ENGINEERS' />   
+            <ShowLink url='/tasks' name='TASKS' />        
+          </Grid>
+        </nav>
+        <div className='content'>
+
+        </div>
+      </div>
+    </HashRouter>
+  )
 }
 
 export default App;
